24d653193948d5d7b754c8f0b38aee5ed3648e37
fifisaac <99099211+fifisaac@users.noreply.github.com>
2025-07-12 16:59:51 +0100

    changed json formatting to allow stop multiple entries for same flight

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from flask import Flask
 import requests
 import json
 
-reg = ['ZZ416', 'ZZ418', 'ZZ419', 'ZZ504', 'ZZ507']
+reg = ['5B-DDO'] # 'ZZ416', 'ZZ418', 'ZZ419', 'ZZ504', 'ZZ507', 
+bbox = {"gaza": [[34.609, 32.820], [30.929, 34.659]],
+        "uk": [[60.479, -10.979], [50.378, 3.515]]}
 
 app = Flask(__name__)
 @app.route('/check')
@@ -10,18 +12,47 @@ def check():
     found = 0
     for i in reg:
         r = requests.get(f'''https://api.adsb.lol/v2/reg/{i}''')
+
         if r.status_code != 200:
             return f'An error occured in the adsb.lol API.', r.status_code
             ## adsb.lol seems to intermittently return 503 errors. adsb.fi or adsb.one may be an alternative
+        
         rjson = r.json()
         if rjson['ac'] != []:
             found += 1
+
             # Need JSON formatting
-            with open('planes.json', 'a') as o:
-                json.dump({'reg': rjson['ac'][0]['r'], 
-                            'lat': rjson['ac'][0]['lat'],
-                            'lon': rjson['ac'][0]['lon'],
-                            'time': rjson['ctime']}, o)
+            with open('planes.json', 'a+') as o:
+                o.seek(0)
+                file = json.loads(o.read())
+                flights = file["flights"]
+                time = rjson['ctime']
+                newFlight = True
+                for j in range(1, len(reg)+1):
+                    if j >= len(flights):
+                        break
+                    
+                    if (flights[-j]["reg"] == i) and (flights[-j]['locs'][-1]["time"] + 12000 <= time):
+                        newFlight = False
+                        flights[-j]['locs'].append({'lat': rjson['ac'][0]['lat'],
+                                            'lon': rjson['ac'][0]['lon'],
+                                            'time': time})
+                        break
+                        
+                if newFlight:
+                    flights.append({
+                                    "reg": i,
+                                    "locs": [
+                                        {
+                                            "lat": rjson['ac'][0]['lat'],
+                                            "lon": rjson['ac'][0]['lon'],
+                                            "time": time
+                                        }
+                                        ]
+                                    })
+
+            with open('woahplanes.json', 'w') as o:
+                json.dump({"flights": flights}, o, indent=4)
 
     return f'Success: {found} planes found'
 
